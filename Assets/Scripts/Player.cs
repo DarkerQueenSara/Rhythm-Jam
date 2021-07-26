21286f1264478c49137a6ce8f945de3bc273b904
Carlos Marques <catralitos11@gmail.com>
2021-07-26 18:31:37 +0100

    Passei algumas coisas para ser UI (e criei um canvas) e fiz funções na classe Player para receber e restaurar dano

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,34 +1,92 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class Player : MonoBehaviour
 {
-    public enum PlayerType {
+    public enum PlayerType
+    {
         PLAYER,
         AI
     }
 
     public PlayerType playerType;
 
-    [Header("AI Options")]
-    [Range(0.0f, 1.0f)]
+    public int maxHealth = 100;
+    [HideInInspector] public int currentHealth;
+    [HideInInspector] public int healthToRemove;
+
+    [Header("AI Options")] [Range(0.0f, 1.0f)]
     public float AIMissChance;
+
     public float AIMissPenalty;
 
-    [Header("Sprites")]
-    public Sprite frontSprite;
+    [Header("Sprites")] public Sprite frontSprite;
     public Sprite backSprite;
 
+    [Header("UI")]  public Image backHealthBar;
+    public Image frontHealthBar;
+    public float healTime = 2f;
+
     private float AIMissPenaltyEndTime = 0f;
-    public bool AIHit() {
-        if(playerType == PlayerType.PLAYER) return false;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+    }
+
+    public void AddDamage(int damage)
+    {
+        healthToRemove += damage;
+        backHealthBar.color = Color.red;
+        //se calhar tenho que usar lerp
+        frontHealthBar.fillAmount -= (float) damage / maxHealth;
+    }
+
+    public void DealDamage()
+    {
+        currentHealth -= healthToRemove;
+        healthToRemove = 0;
+        //se calhar tenho que usar lerp
+        backHealthBar.fillAmount = frontHealthBar.fillAmount;
+    }
+
+    public void RestoreHealth(int health)
+    {
+        currentHealth += health;
+        backHealthBar.color = Color.yellow;
+        //se calhar tenho que usar lerp
+        backHealthBar.fillAmount += (float) health / maxHealth;
+        Invoke(nameof(FinishHeal), healTime);
+    }
+
+    private void FinishHeal()
+    {
+        //se calhar tenho que usar lerp
+        frontHealthBar.fillAmount = backHealthBar.fillAmount;
+    }
+
+
+    public bool AIHit()
+    {
+        if (playerType == PlayerType.PLAYER) return false;
 
         float hit = Random.Range(0f, 1f);
-        if(hit > AIMissChance && Time.time > AIMissPenaltyEndTime) {
+        if (hit > AIMissChance && Time.time > AIMissPenaltyEndTime)
+        {
             //hit
             return true;
-        } else {
+        }
+        else
+        {
             //miss
             AIMissPenaltyEndTime = Time.time + AIMissPenalty;
             return false;
